a798e7cb0e1f87bd49a91eed519bf47dc9f08aef
D1aI3L0 <71220227+D1aI3L0@users.noreply.github.com>
2022-11-14 20:03:57 +0300

    Update main.cpp

--- Lab_4/main.cpp
+++ Lab_4/main.cpp
@@ -1,17 +1,25 @@
-#include "RightTriangle.h"
-#include "Circle.h"
-#include "Queue.h"
+#include "Figures/Figures.h"
 
 int Figure::ID = 0;
 
 int main()
 {
-    Queue<Circle> c;
-    Circle c1, c2, c3, c4;
-    cin >> c1 >> c2 >> c3 >> c4;
-    c.pushPos(c1, 4);
-    c.pushPos(c2,1);
-    c.pushPos(c3, 2);
-    c.pushPos(c4, 1);
-    return 0;
+    Figure* figure;
+
+    Circle* circle = new Circle;
+    cin >> *circle;
+    figure = circle;
+    figure->Show();
+    cout << endl;
+
+    Triangle* triangle = new Triangle;
+    cin >> *triangle;
+    figure = triangle;
+    figure->Show();
+    cout << endl;
+
+    RightTriangle* rightTriangle = new RightTriangle;
+    cin >> *rightTriangle;
+    figure = rightTriangle;
+    figure->Show();
 }
