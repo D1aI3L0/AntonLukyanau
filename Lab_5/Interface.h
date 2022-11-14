13141204ae20d1bd78f71b95f465af99368412da
D1aI3L0 <71220227+D1aI3L0@users.noreply.github.com>
2022-11-14 19:34:51 +0300

    Update Interface.h

--- Lab_5/Interface.h
+++ Lab_5/Interface.h
@@ -1,10 +1,8 @@
 #ifndef LAB5_INTERFACE_H
 #define LAB5_INTERFACE_H
 
-#include "Templates/Matrix.h"
-#include "Templates/Queue.h"
-#include "../Lab4/Circle.h"
-#include "../Lab4/RightTriangle.h"
+#include "Templates/Templates.h"
+#include "../Lab4/Figures.h"
 
 
 class Interface{
